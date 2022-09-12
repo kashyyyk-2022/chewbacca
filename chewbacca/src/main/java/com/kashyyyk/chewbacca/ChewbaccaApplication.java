82704d070412e727a5cbe7f12c8b7fad7e57b3ff
Wincent Holm <wincent.holm@gmail.com>
2022-09-12 14:35:18 +0200

    Comments the main method This commit comments the main method of the application.

--- chewbacca/src/main/java/com/kashyyyk/chewbacca/ChewbaccaApplication.java
+++ chewbacca/src/main/java/com/kashyyyk/chewbacca/ChewbaccaApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChewbaccaApplication {
 
+	/**
+	 * This is the main method of the application.
+	 * It does not need to be modified for the most part.
+	 * 
+	 * If there is any additional configuration that needs to be done,
+	 * or if there needs to be some initialization code, it can be done here.
+	 * 
+	 * @param args		Arguments passed to the application
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ChewbaccaApplication.class, args);
 	}
