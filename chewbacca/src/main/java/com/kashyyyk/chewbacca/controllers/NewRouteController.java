0b70214558439c4ae3f25b7ddda9671c1183a9ec
Vhagenbo <98605236+Hagenbo@users.noreply.github.com>
2022-09-22 08:02:25 -0400

    Create NewRouteController.java

---
+++ chewbacca/src/main/java/com/kashyyyk/chewbacca/controllers/NewRouteController.java
@@ -0,0 +1,15 @@
+package com.kashyyyk.chewbacca.controllers;
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class NewRouteController {
+    @GetMapping("newroute")
+    public ModelAndView getIndex(Model model) {
+        return new ModelAndView("html_pages/newroute.html", model.asMap());
+    }
+}
