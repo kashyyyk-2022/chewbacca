62d6bedf3e3b5668e03359410630af31732defae
rstandar <rassta@student.chalmers.se>
2022-09-22 11:35:26 +0200

    Created spring-controller currentRouteController for CurrentRoute

---
+++ chewbacca/src/main/java/com/kashyyyk/chewbacca/controllers/currentRouteController.java
@@ -0,0 +1,13 @@
+package com.kashyyyk.chewbacca.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class currentRouteController {
+    @GetMapping("CurrentRoute")
+    public ModelAndView getIndex(Model model){
+        return new ModelAndView("html_pages/CurrentRoute.html", model.asMap());
+    }
+}
