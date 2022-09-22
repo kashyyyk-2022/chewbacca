a9bfcc6f0fc86897a6e02d4db394871c08a83c71
Oscar <oscarmar@student.chalmers.se>
2022-09-22 14:00:29 +0200

    Updated IndexController.java to contain default cookie values and changed its structure to resemble that of SettingsController.java

--- chewbacca/src/main/java/com/kashyyyk/chewbacca/controllers/IndexController.java
+++ chewbacca/src/main/java/com/kashyyyk/chewbacca/controllers/IndexController.java
@@ -1,15 +1,34 @@
 package com.kashyyyk.chewbacca.controllers;
 
+import com.kashyyyk.chewbacca.locale.LocaleResources;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 @Controller
 public class IndexController {
 
     @GetMapping("")
-    public ModelAndView getIndex(Model model){
+    public ModelAndView getIndex(
+            Model model,
+            @CookieValue(value = "unit", defaultValue = "metric") String unit,
+            @CookieValue(value = "lang", defaultValue = "en") String lang,
+            @CookieValue(value = "darkMode", defaultValue = "false") String darkMode
+    ){
+        model.addAttribute("unit", unit);
+        model.addAttribute("lang", lang);
+        model.addAttribute("darkMode", darkMode);
+
+        try {
+            LocaleResources.getLocales(lang).populateModel(model);
+        } catch (IOException e) {
+            return new ModelAndView("html_pages/error.html", model.asMap());
+        }
+
         return new ModelAndView("html_pages/index.html", model.asMap());
     }
 }
