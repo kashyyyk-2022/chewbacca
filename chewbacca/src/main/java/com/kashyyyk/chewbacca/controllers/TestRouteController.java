45fa3604627170f9031c09f73077c3826d455a58
Pipipipiano <marcusphu@gmail.com>
2022-09-23 14:51:24 +0200

    Update TestRouteController.java
    Removed old testing code.

--- chewbacca/src/main/java/com/kashyyyk/chewbacca/controllers/TestRouteController.java
+++ chewbacca/src/main/java/com/kashyyyk/chewbacca/controllers/TestRouteController.java
@@ -1,8 +1,5 @@
 package com.kashyyyk.chewbacca.controllers;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Response;
-import org.apache.catalina.connector.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +13,4 @@ public class TestRouteController {
         return new ModelAndView("html_pages/testroute.html", model.asMap());
     }
 
-
-    OkHttpClient client = new OkHttpClient();
-    Request request = new Request.Builder()
-            .url("https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&profile=car&locale=de&calc_points=false&key=api_key")
-            .get()
-            .build();
-
-    Response response = client.newCall(request).execute();
 }
