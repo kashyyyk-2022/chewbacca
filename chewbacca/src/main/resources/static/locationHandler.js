ffb4da2ed06223f45265fe367264a2e08dfff0bc
Vhagenbo <98605236+Hagenbo@users.noreply.github.com>
2022-09-27 16:03:54 -0400

    Added a modal when clicking "New Route" asking for permission to access location. As well as some comments in newroute.html (nothing important)

---
+++ chewbacca/src/main/resources/static/locationHandler.js
@@ -0,0 +1,12 @@
+function getLocation(longOrLat) {
+    var cookie = getCookie('locationPermission',false);
+    if (cookie == false) {
+
+    }
+    <!--
+            if(permission == false):Ask permission to access user location
+                   deny: reload index
+                   allow: check if gps is turned on.
+                        if not turned on: ask to turn on.
+                            else get location: save longitude and latitude in-->
+}
