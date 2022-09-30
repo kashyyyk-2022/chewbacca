be09a79fd1cb4b462b282b7213e501a3ca42d1e1
Pipipipiano <marcusphu@gmail.com>
2022-09-30 14:02:04 +0200

    Updated currentRoute and testRoute
    Updated currentRoute and testRoute with a green and a red marker for start and goal.
    
    Also removed unnecessary code in testRoute.

--- chewbacca/src/main/resources/static/generateRoute.js
+++ chewbacca/src/main/resources/static/generateRoute.js
@@ -18,10 +18,10 @@ const waypoints = [
 
 //Marks the first position (start) in the array with a blue marker.
 var startPos = L.marker(waypoints[0]).addTo(map); //NOT THE CURRENT POSITION, ONLY A MOCK POS.
-
+startPos._icon.classList.add("huechangeStart");
 //Marks the last position (goal) in the array with a red marker.
 var endPos = L.marker(waypoints[waypoints.length-1]).addTo(map);
-endPos._icon.classList.add("huechange");
+endPos._icon.classList.add("huechangeGoal");
 
 for(let i = 0; i < waypoints.length; i++){
     L.polyline([
