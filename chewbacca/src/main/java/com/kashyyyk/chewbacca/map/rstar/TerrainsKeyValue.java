6b7104a8fe7c12408f90608ef71572b4287547b4
rstandar <rassta@student.chalmers.se>
2022-10-12 17:43:47 +0200

    Updated Javadoc for TerrainsKeyValue to make the class more understandable

--- chewbacca/src/main/java/com/kashyyyk/chewbacca/map/rstar/TerrainsKeyValue.java
+++ chewbacca/src/main/java/com/kashyyyk/chewbacca/map/rstar/TerrainsKeyValue.java
@@ -1,5 +1,6 @@
 package com.kashyyyk.chewbacca.map.rstar;
 
+/** Class used to store settings for different terrains, by associating the terrains with a set of key-value pairs */
 public class TerrainsKeyValue {
 
     /*Setup of key-value pairs that are suitable for terrain urban*/
@@ -28,6 +29,10 @@ public class TerrainsKeyValue {
     };
     private static final KeyValue water = new KeyValue(wat);
 
+    /** Method to get a KeyVal structure containing keys and values that is worth checking inside osm-tags for a specific terrain
+     * @param terrain - string with one of the preferences: "water","nature" or "urban".
+     * @return - a KeyValue object, with the keys and values that are relevant for the terrain preferred by the user.
+     */
     public static KeyValue getTerrainKV(String terrain){
         KeyValue result = switch (terrain) {
             case "water" -> water;
