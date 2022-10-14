f2a18c88e0cc02e70c40d04a9e042fc49ae0ceb5
lovix182 <lovisaro@student.chalmers.se>
2022-10-14 10:34:20 +0200

    added background to dark mode theme. Centered text in about.html

--- chewbacca/src/main/resources/static/darkModeHandler.js
+++ chewbacca/src/main/resources/static/darkModeHandler.js
@@ -1,7 +1,8 @@
 function toggleDarkMode() {
+
     let darkMode = getCookie('darkMode');
     if (darkMode == "true") {
-        document.body.style.backgroundColor = "black";
+        document.body.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg')";
         document.body.style.color="white"
 
         /*Script to toggle color of element to dark mode. Add name of class inside one of the selectors by
@@ -15,7 +16,9 @@ function toggleDarkMode() {
             cards[i].style.backgroundColor = "darkgray";
         }
     } else {
-        document.body.style.backgroundColor = "white";
+        document.body.style.backgroundImage = "url('https://cdn.wallpapersafari.com/91/60/M9k5Ka.png')";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundSize = "cover";
         document.body.style.color="black"
 
         /*Script to toggle color of element to light mode. Add name of class inside one of the selectors by
